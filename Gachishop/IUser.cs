29460af6ff4474bcb5d990e2b58b47c111cf0a4d
ukrainian-developer <plotva10@gmail.com>
2022-04-26 22:51:53 +0300

    Added user interface and class

---
+++ Gachishop/IUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gachishop;
+
+public interface IUser
+{
+    [Key]
+    int Id { get; set; }
+    string Name { get; set; }
+    string Password { get; set; }
+    bool IsAdmin { get; set; }
+    bool CheckAccess(string name, string password);
+}
---
+++ Gachishop/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gachishop;
+
+public class User : IUser
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public bool IsAdmin { get; set; }
+    public bool CheckAccess(string name, string password)
+    {
+        return (Name == name) && (Password == password);
+    }
+}
