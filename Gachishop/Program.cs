26e6eaaa0d07561229ee46a5a95dd4f445eb5179
ukrainian-developer <plotva10@gmail.com>
2022-04-28 15:04:37 +0300

    Changed Program.cs

--- Gachishop/Program.cs
+++ Gachishop/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using Gachishop;
+using Microsoft.EntityFrameworkCore;
 
 class Program
 {
     static void Main(string[] args)
     {
+        UserContext userContext = new UserContext();
+        
+        userContext.Users.Add(new User("Van", "123123123", true, 20));
         Console.WriteLine("Hello, world");
     }
 }
