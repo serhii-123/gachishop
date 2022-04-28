4e928b870e318db95f474daf0fc4b14fcc841e9e
ukrainian-developer <plotva10@gmail.com>
2022-04-28 14:59:04 +0300

    Created context

---
+++ Gachishop/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Gachishop;
+
+public class UserContext :DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public UserContext()
+    {
+        Database.EnsureCreated();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Filename=User.db");
+    }
+}
