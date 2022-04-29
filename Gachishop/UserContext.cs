c5677383741be1c0fc13c9eb65ae70eee06bca7d
ukrainian-developer <plotva10@gmail.com>
2022-04-29 13:13:21 +0300

    Some changes

--- Gachishop/UserContext.cs
+++ Gachishop/UserContext.cs
@@ -2,15 +2,22 @@
 
 namespace Gachishop;
 
-public class UserContext :DbContext
+public class UserContext : DbContext
 {
     public DbSet<User> Users { get; set; }
     public UserContext()
     {
-        Database.EnsureCreated();
+        
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasKey(u => u.Id);
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Filename=User.db");
+        base.OnConfiguring(optionsBuilder);
     }
 }
