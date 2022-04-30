27dc2806b47dfdca51a51079b2c2d43977e5c10a
ukrainian-developer <plotva10@gmail.com>
2022-04-30 17:09:45 +0300

    Changed Program.cs

--- Gachishop/Program.cs
+++ Gachishop/Program.cs
@@ -6,7 +6,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        string[] productTypes = { "Bondage", "Latex glove" };
+        AdminService adminService = new AdminService(productTypes);
         LoginService loginService = new LoginService();
         loginService.Login();
+        
+        if (loginService.AuthorizedUser.IsAdmin)
+        {
+            adminService.Start();
+        }
     }
 }
