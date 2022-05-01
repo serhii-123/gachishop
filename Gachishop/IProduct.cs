a19053a1526ab15697c1ff4fb553ef8588008318
ukrainian-developer <plotva10@gmail.com>
2022-05-01 17:47:39 +0300

    Added class and interface

---
+++ Gachishop/IProduct.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gachishop;
+
+public interface IProduct
+{
+    [Key]
+    int Id { get; set; }
+    string Name { get; set; }
+    string Description { get; set; }
+    string Type { get; set; }
+    int Price { get; set; }
+    int NumberOfUnits { get; set; }
+    int Discount { get; set; }
+}
---
+++ Gachishop/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gachishop;
+
+public class Product : IProduct
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Type { get; set; }
+    public int Price { get; set; }
+    public int NumberOfUnits { get; set; }
+    public int Discount { get; set; }
+
+    public Product(string name, string description, string type, int price, int numberOfUnits, int discount)
+    {
+        Name = name;
+        Description = description;
+        Type = type;
+        Price = price;
+        NumberOfUnits = numberOfUnits;
+        Discount = discount;
+    }
+}
