50a5e851c3988cb8f6bb170b6aa404192d0bb98f
ukrainian-developer <plotva10@gmail.com>
2022-05-02 13:29:25 +0300

    Some changes

--- Gachishop/Product.cs
+++ Gachishop/Product.cs
@@ -9,7 +9,22 @@ public class Product : IProduct
     public string Name { get; set; }
     public string Description { get; set; }
     public string Type { get; set; }
-    public int Price { get; set; }
+
+    public int Price
+    {
+        get
+        {
+            return _price;
+        }
+        set
+        {
+            if (value != 0)
+            {
+                _price = value;
+            }
+            else throw new Exception("Value is 0");
+        }
+    }
     public int Quantity
     {
         get
@@ -18,13 +33,33 @@ public class Product : IProduct
         }
         set
         {
-            
+            if (value != 0)
+            {
+                _quantity = value;
+            }
+            else throw new Exception("Value is 0");
         }
     }
 
-    public int Discount { get; set; }
-    private int _quantity;
+    public int Discount
+    {
+        get
+        {
+            return _discount;
+        }
+        set
+        {
+            if (value <= 100)
+            {
+                _discount = value;
+            }
+            else throw new Exception("Value is bigger than 100");
+        }
+    }
 
+    private int _price;
+    private int _quantity;
+    private int _discount;
     public Product(string name, string description, string type, int price, int quantity, int discount)
     {
         Name = name;
