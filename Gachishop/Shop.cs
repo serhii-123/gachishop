cfab67e824672fa0a0b53a313f031f43b406a689
ukrainian-developer <plotva10@gmail.com>
2022-05-06 17:50:40 +0300

    Added shop context

---
+++ Gachishop/Shop.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Gachishop;
+
+public class Shop : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasKey(u => u.Id);
+        modelBuilder.Entity<Product>().HasKey(p => p.Id);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Filename=Shop.db");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
