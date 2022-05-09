b2c787c3d0cada7c379aae77d554e58da0fa519b
ukrainian-developer <plotva10@gmail.com>
2022-05-09 11:56:10 +0300

    Added data parsers

---
+++ Gachishop/AdminServiceDataParser.cs
@@ -0,0 +1,112 @@
+﻿namespace Gachishop;
+
+public static class AdminServiceDataParser
+{
+    public static string GetProductName()
+    {
+        string name;
+        Console.WriteLine("Введите имя товара");
+        name = CustomInput.ReadText();
+
+        while (true)
+        {
+            if (name.Length < 3)
+            {
+                Console.WriteLine("Слишком короткое имя. Введите другое");
+                name = CustomInput.ReadText();
+            }
+            else return name;
+        }
+    }
+    public static string GetProductDescription()
+    {
+        string description;
+        Console.WriteLine("Введите описание товара");
+        description = CustomInput.ReadText();
+
+        while (true)
+        {
+            if (description.Length < 20)
+            {
+                Console.WriteLine("Слишком короткое описание. Введите другое");
+                description = CustomInput.ReadText();
+            }
+            else return description;
+        }
+    }
+
+    public static string GetProductCategory()    
+    {
+        string type;
+        string[] productCategories;
+        
+        using (ShopContext ctx = new ShopContext())
+        {
+            productCategories = ctx.ProductCategories.Select(c => c.Name).ToArray();
+        }
+
+        Console.WriteLine("Введите категорию товара");
+        type = CustomInput.ReadText();
+        
+        while (true)
+        {
+            if (!productCategories.Contains(type))
+            {
+                Console.WriteLine("Неверный тип товара. Введите другой");
+                type = CustomInput.ReadText();
+            }
+            else return type;
+        }
+    }
+
+    public static int GetProductPrice()
+    {
+        int price;
+        Console.WriteLine("Введите цену товара");
+        price = CustomInput.ReadNumber();
+
+        while (true)
+        {
+            if (price == 0)
+            {
+                Console.WriteLine("Цена не может быть равна нулю. Введите другую");
+                price = CustomInput.ReadNumber();
+            }
+            else return price;
+        }
+    }
+    
+    public static int GetProductQuantity()
+    {
+        int quantity;
+        Console.WriteLine("Введите кол-во единиц товара");
+        quantity = CustomInput.ReadNumber();
+
+        while (true)
+        {
+            if (quantity == 0)
+            {
+                Console.WriteLine("Кол-во товаров не может быть равно нулю. Введите другое");
+                quantity = CustomInput.ReadNumber();
+            }
+            else return quantity;
+        }
+    }
+    
+    public static int GetProductDiscount()
+    {
+        int discount;
+        Console.WriteLine("Введите скидку на товар");
+        discount = CustomInput.ReadNumber();
+
+        while (true)
+        {
+            if (discount > 99)
+            {
+                Console.WriteLine("Скидка не может быть больше 99-ти процентов. Введите другую");
+                discount = CustomInput.ReadNumber();
+            }
+            else return discount;
+        }
+    }
+}
---
+++ Gachishop/BuyerServiceDataParser.cs
@@ -0,0 +1,64 @@
+﻿namespace Gachishop;
+
+public static class BuyerServiceDataParser
+{
+    public static int GetProductId()
+    {
+        int id;
+        
+        Console.WriteLine("Введите номер товара");
+        id = CustomInput.ReadNumber();
+
+        using (ShopContext ctx = new ShopContext())
+        {
+            while (true)
+            {
+                Product product = ctx.Products.FirstOrDefault(p => p.Id == id);
+                if (product == null)
+                {
+                    Console.WriteLine("Ошибка! Нет товара с таким номером. Введите другой");
+                    id = CustomInput.ReadNumber();
+                    continue;
+                }
+                
+                bool quantityIsNotNull = ctx.ProductInventories.First(c => c.Id == product.CategoryId).Quantity > 0;
+
+                if (!quantityIsNotNull)
+                {
+                    Console.WriteLine("Ошибка! Данный товар сейчас не доступен. Введите другой номер");
+                    id = CustomInput.ReadNumber();
+                    continue;
+                }
+                
+                break;
+            }
+        }
+        
+        return id;
+    }
+
+    public static int GetProductQuantity(int productId)
+    {
+        int quantity, currentQuantity;
+        Product product;
+
+        using (ShopContext ctx = new ShopContext())
+        {
+            product = ctx.Products.First(p => p.Id == productId);
+            currentQuantity = ctx.ProductInventories.First(i => i.Id == product.InventoryId).Quantity;
+        }
+
+        Console.WriteLine("Введите кол-во товаров");
+        quantity = CustomInput.ReadNumber();
+
+        while (true)
+        {
+            if (quantity > currentQuantity)
+            {
+                Console.WriteLine("Ошибка! Недоступно такое количество. Введите другое");
+                quantity = CustomInput.ReadNumber();
+            }
+            else return quantity;
+        }
+    }
+}
