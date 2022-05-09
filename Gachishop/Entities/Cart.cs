5b2e44458c2569e741923f46f4ea4bb69866976b
ukrainian-developer <plotva10@gmail.com>
2022-05-09 13:28:58 +0300

    Added new entities

---
+++ Gachishop/Entities/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gachishop;
+
+public class Cart
+{
+    [Key]
+    public int Id { get; set; }
+    public int UserId { get; set; }
+
+    public Cart(int userId)
+    {
+        UserId = userId;
+    }
+}
---
+++ Gachishop/Entities/CartItem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gachishop;
+
+public class CartItem
+{
+    [Key]
+    public int Id { get; set; }
+    public int CartId { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+
+    public CartItem(int cartId, int productId, int quantity)
+    {
+        CartId = cartId;
+        ProductId = productId;
+        Quantity = quantity;
+    }
+}
